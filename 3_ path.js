0a4eaee00f92b700f8b6015270c194ef858730f7
Corvax <vlvete1990@yandex.ru>
2020-06-28 12:01:54 +0300

    chore: add path

--- 3_ path.js
+++ 3_ path.js
@@ -6,3 +6,14 @@ console.log(path.basename(file))
 console.log(path.basename(file, '.txt'))
 console.log(path.dirname(file))
 console.log(path.extname(file))
+
+const folderName = 'temp'
+
+// join
+console.log(path.join('/', folderName, 'template.txt'))
+
+// resolve
+console.log(path.resolve('template.txt'))
+console.log(path.resolve(folderName, 'template.txt'))
+console.log(path.resolve('/temp', 'template.txt'))
+
