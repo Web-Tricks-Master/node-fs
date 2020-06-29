99599dfa3ff61d50949b48c78b213085a483ba2e
Corvax <vlvete1990@yandex.ru>
2020-06-29 17:44:01 +0300

    chore: add dir

---
+++ 4_dir.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const file = 'package.json';
+
+// проверка существования файла
+fs.access(file, fs.constants.F_OK, (err) => {
+  console.log(`${file} ${err ? 'не существует' : 'существует'}`);
+});
+
+// проверка на возможность прочитать файла
+fs.access(file, fs.constants.R_OK, (err) => {
+    console.log(`${file} ${err ? 'не прочитать' : 'прочитать'}`);
+});
+  
+// проверка на возможность перезаписать файла
+fs.access(file, fs.constants.W_OK, (err) => {
+    console.log(`${file} ${err ? 'не перезаписать' : 'перезаписать'}`);
+});
